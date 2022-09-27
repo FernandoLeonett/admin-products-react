6394bfd82116a3d2e66be65bdcc139e4b5182972
matiascabral <matiascabralmendez@gmail.com>
2022-09-27 18:47:20 -0300

    test

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
+import { CLOUD_NAME } from "../../util/util";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import "./Layout.css";
 
 const Layout = ({ children }) => {
-
   return (
     <>
       <Header />
-
       {/* {
         setIsReadyForInstall &&
         <button onClick={downloadApp}> Descargasdasda </button>
       } */}
+      CLOUD_NAME:{CLOUD_NAME}
       <main>{children}</main>
       <Footer />
     </>
